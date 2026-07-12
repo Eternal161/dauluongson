fe36d8e3fa40f6d2278044e8cb4eb73ddd88226c
Eternal161 <daulam1601@gmail.com>
2026-07-12 21:54:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ from playwright_stealth import Stealth
 # =========================================================
 # CONFIG LƯƠNG SƠN TV - BẢN FULL CHỐNG ĐẠN
 # =========================================================
-TARGET_SITE   = "https://ls91.site/"
-BASE_URL      = "https://ls91.site"
+TARGET_SITE   = "https://luongson171.pro/"
+BASE_URL      = "https://luongson171.pro"
 FILE_PATH     = "luongson.json"
 LIMIT_MATCHES = 15 # Tăng số lượng để hiện được nhiều trận sắp tới hơn
 
