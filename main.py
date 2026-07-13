1e608ed6d048e0c409a7e61aa54ea1f4430e3eb5
Eternal161 <daulam1601@gmail.com>
2026-07-13 07:17:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ def apply_stealth(page):
 # =========================================================
 # CONFIG LƯƠNG SƠN TV
 # =========================================================
-TARGET_SITE   = "https://luongson171.pro"
-BASE_URL      = "https://luongson171.pro"
+TARGET_SITE   = "https://luongson-wc2026.com/"
+BASE_URL      = "https://luongson-wc2026.com"
 FILE_PATH     = "luongson.json"
 LIMIT_MATCHES = 15 
 
